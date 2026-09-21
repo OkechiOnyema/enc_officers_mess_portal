00b18d6910a4fbdd0dd2ab277747b4193610de67
OkechiOnyema <okechionyema@gmail.com>
2026-09-21 13:10:03 +0100

    Initial working commit

--- enc_officers_mess_portal/settings.py
+++ enc_officers_mess_portal/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'members',
+    'staff',
+    'finance',
+    'requests',
+    'rest_framework',   # for APIs
 ]
 
 MIDDLEWARE = [
